6f7938ce0b002ac31f556b335252d5a6d078f2ae
Alterlapsus <israel.delen96@gmail.com>
2022-09-08 07:04:54 -0600

    Tipos de variables

--- 01-Fundamentos/App.js
+++ 01-Fundamentos/App.js
@@ -1,3 +1,20 @@
+//console.log('Hola mundo');
 
+let a = 10; 
+let b = 20; 
+let c = 30; 
+let d = 40; 
+let x = a + b; 
 
-console.log('Hola mundo');
+console.log('a',a);
+console.log('b',b);
+console.log('c',c);
+
+// formas de mostrar datos en consola
+
+// console.log(x);
+// console.warn(x);
+// console.error(x);
+// console.info(x);
+
+// console > objeto  log > metodo
