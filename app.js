c4cd7ba53dfeba6f89524acbfd16cdcbe3a03e8c
Alterlapsus <israel.delen96@gmail.com>
2022-09-08 14:52:34 -0600

    Realizando creación de carpetas

---
+++ app.js
@@ -0,0 +1,37 @@
+alert('Hola desde app.js')
+// console.log('Hola mundo');
+
+// let a = 10; 
+// let b = 20; 
+// let c = 30; 
+// let d = 40; 
+// let x = a + b; 
+
+// otra forma de realizarlo 
+
+let a = 10,
+    b = 20, 
+    c = 'Hola ', 
+    d = 'Spiderman', 
+    x = a + b; 
+
+    const saludo = c + d;
+
+// console.log('%c Mis variables', 'color:blue; font-weight: bold;')
+// console.log({a});
+// console.log({b});
+// console.log({c});
+
+c = 'Hola de nuevo';
+
+// console.table({a, b, c, d, x});
+
+
+// formas de mostrar datos en consola
+
+// console.log(x);
+// console.warn(x);
+// console.error(x);
+// console.info(x);
+
+// console > objeto  log > metodo 
