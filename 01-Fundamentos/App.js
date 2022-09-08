810f718f540c8065082a321f7c22fd634344ee51
Alterlapsus <israel.delen96@gmail.com>
2022-09-08 14:17:40 -0600

    aprendiendo sobre variables

--- 01-Fundamentos/App.js
+++ 01-Fundamentos/App.js
@@ -1,14 +1,28 @@
 //console.log('Hola mundo');
 
-let a = 10; 
-let b = 20; 
-let c = 30; 
-let d = 40; 
-let x = a + b; 
+// let a = 10; 
+// let b = 20; 
+// let c = 30; 
+// let d = 40; 
+// let x = a + b; 
 
-console.log('a',a);
-console.log('b',b);
-console.log('c',c);
+// otra forma de realizarlo 
+
+let a = 10,
+    b = 20, 
+    c = 'Hola ', 
+    d = 'Spiderman', 
+    x = a + b; 
+
+    const saludo = c + d;
+
+// console.log('%c Mis variables', 'color:blue; font-weight: bold;')
+// console.log({a});
+// console.log({b});
+// console.log({c});
+
+// console.table({a, b, c, d, x});
+
 
 // formas de mostrar datos en consola
 
