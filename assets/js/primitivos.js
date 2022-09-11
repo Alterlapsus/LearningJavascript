60f1f7ffadf40961b48c96d4d4d5226454193e49
Alterlapsus <israel.delen96@gmail.com>
2022-09-10 19:56:48 -0600

    tipos primitivos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,17 @@
+let nombre = 'Peter Parker';
+console.log( nombre);
+
+nombre = 'Ben Parker';
+console.log( nombre);
+
+nombre = "Tía May";
+nombre = `Tía May`;
+
+console.log( typeof nombre);
+
+nombre = 123;
+console.log( typeof nombre);
+
+// booleanos 
+
+let esMarvel = false;
