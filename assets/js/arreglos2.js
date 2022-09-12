9c8a8a4b76ddefc26c3ca7b5d3665c4d183c1b7f
Alterlapsus <israel.delen96@gmail.com>
2022-09-12 16:25:56 -0600

    Aprendiendo sobre arreglos 2

--- assets/js/arreglos2.js
+++ assets/js/arreglos2.js
@@ -1,2 +1,34 @@
-let juegos = ['Zelda', 'Mario', 'Mtroid', 'Chrono'];
+let juegos = ['Zelda', 'Mario', 'Metroid', 'Chrono'];
 console.log('Largo;', juegos.length );
+
+let primero = juegos[2 - 2];
+let ultimo = juegos[ juegos.length - 1];
+
+console.log( primero, ultimo);
+
+juegos.forEach( (elemento, indice, arr) => {
+      console.log({elemento, indice, arr });
+});
+
+// agrega array al final
+let nuevaLongitud = juegos.push( 'F-Zero');
+console.log({nuevaLongitud, juegos});
+
+// agrega array al principio
+nuevaLongitud = juegos.unshift('Fire Emblem');
+console.log({nuevaLongitud, juegos});
+
+// Eliminar un array
+let juegoBorrado = juegos.pop();
+console.log({ juegoBorrado, juegos});
+
+//eliminar array no importando su posición
+let pos = 1; 
+
+console.log( juegos);
+let juegosBorrados = juegos.splice( pos, 2);
+console.log({ juegosBorrados, juegos});
+
+// con indexOf si sale -1 es porque no encontro el array, si sale 1 es porque encontró el array
+let metroidIndex = juegos.indexOf('Metroid');
+console.log({ metroidIndex});
