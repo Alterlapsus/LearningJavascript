bc06f1f9846f09b20f972cfe94d5d55a89ff33d6
Alterlapsus <israel.delen96@gmail.com>
2022-09-12 14:57:28 -0600

    Realizando arreglos

---
+++ assets/js/arreglos.js
@@ -0,0 +1,26 @@
+// const arr = new Array(10);
+// console.log(arr);
+
+
+let videoJuegos = ['Mario','Megaman','Chrono Trigger'];
+console.log( videoJuegos);
+
+console.log( videoJuegos [0]);
+
+let arregloCosas = [
+    true, 
+    123, 
+    'Israel',
+    1 + 2,
+    function(){},
+    ()=>{},
+    {a: 1},
+    ['X', 'Megaman', 'Zero', 'Dr. Light',[
+        'Dr. Willy',
+        'Woodman'
+    ]]
+];
+
+// console.log( arregloCosas);
+// console.log( arregloCosas[2]);
+console.log( arregloCosas[7][4][1]);
