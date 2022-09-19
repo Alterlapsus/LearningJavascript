1062d4269907f9e637155c4939adda07683a9703
Alterlapsus <israel.delen96@gmail.com>
2022-09-19 06:55:38 -0600

    Aprendiendo sobre objetos literales

---
+++ assets/js/objeto-literal.js
@@ -0,0 +1,15 @@
+let personaje = {
+    nombre: 'Tony Stark',
+    codeName: 'IronMan',
+    vivo: false, 
+    edad: 40, 
+    coords: {
+        lat: 34.034,
+        lng: -118.70
+
+    },
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+};
+
+console.log(personaje);
+console.log('nombre',personaje.nombre)
