4596063f687117c996e70c6a0b5ee384752e1506
Alterlapsus <israel.delen96@gmail.com>
2022-09-19 13:58:03 -0600

    objeto literal

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -1,4 +1,4 @@
-let personaje = {
+const personaje = {
     nombre: 'Tony Stark',
     codeName: 'IronMan',
     vivo: false, 
@@ -12,7 +12,8 @@ let personaje = {
     direccion: {
      zip: '10880, 90265',
      ubicacion: 'Malibu, California',   
-    }
+    },
+    'ultima_pelicula': 'Infinity War'
 };
 
 console.log(personaje );
@@ -20,4 +21,26 @@ console.log('nombre',personaje.nombre );
 console.log('Nombre', personaje['nombre']);
 console.log('Edad', personaje.edad );
 console.log('coords',personaje.coords.lat );
-console.log('direccion', personaje.direccion );
+console.log('direccion', personaje.direccion.zip );
+
+console.log('No. trajes', personaje.trajes.length);
+console.log('Último Traje', personaje.trajes[personaje.trajes.length - 1]);
+
+const x = 'vivo';
+console.log('vivo', personaje[x]);
+
+console.log('ultima pelicula', personaje['ultima_pelicula']);
+
+// mas detalles
+
+delete personaje.edad; 
+console.log( personaje);  
+
+personaje.casado = true;
+
+const entriesPares = Object.entries(personaje);
+console.log( entriesPares);
+
+personaje = true;
+
+console.log( personaje); 
