dd99383cc55a4bfd79f3168a45eea8a83c515c24
Alterlapsus <israel.delen96@gmail.com>
2022-09-20 09:22:21 -0600

    retorno de funciones

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,9 +1,30 @@
-function saludar(){
-    console.log('Hola Mundo');
+function saludar(nombre){
+    // console.log(arguments);
+    // console.log('Hola ' + nombre);
+    return [1, 2];
+
+    // esto nunca se va ha ejecutar    
+    console.log('Soy un código que esta depués del return');
 }
 
 
+const saludar2 = function( nombre ){
+    console.log('Hola ' + nombre);
+}
 
-saludar();
-saludar();
-saludar();
+const saludarFlecha = () => {
+    console.log('Hola Flecha');
+}
+
+const saludarFlecha2 = ( nombre ) => {
+    console.log('Hola ' + nombre);
+}
+
+
+const retornoDeSaludar = saludar( 'Israel', 40, true, 'Guatemala' );
+console.log( retornoDeSaludar [0], retornoDeSaludar [1]);
+// saludar2( 'Israel' );
+
+// saludarFlecha();
+// saludarFlecha2('Israel');
+
