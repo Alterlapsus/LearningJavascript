2930fb9e191b3fdcdcdf15a9037703c2b0d568e4
Alterlapsus <israel.delen96@gmail.com>
2022-09-21 16:29:47 -0600

    aprendiendo sobre operadores ternarios

--- assets/js/operador-ternario.js
+++ assets/js/operador-ternario.js
@@ -7,12 +7,14 @@ pero los fines de semana abrimos a las 9
 //Entra a un sitio web, para consultar si está abierto hoy...
 
 const dia = 1; //0: domingo... 1: Lunes...
-const horaActual = 10;  
+const horaActual = 11;  
 
 let horaApertura;
 let mensaje; // está abierto, está cerrado, hoy abrimos a las xx
 
-if ( dia === 0 || dia === 6){
+// if ( dia === 0 || dia === 6){
+// ota forma de realizarlo
+if ( [0,6] ){ 
     console.log('Fin de semana');
     horaApertura = 9;
 }else{
