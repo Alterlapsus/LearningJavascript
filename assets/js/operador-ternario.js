4fad65cd7bfa55c25d15f60b1ef7899e5d972401
Alterlapsus <israel.delen96@gmail.com>
2022-09-21 15:38:24 -0600

    Operador ternario

---
+++ assets/js/operador-ternario.js
@@ -0,0 +1,15 @@
+/*
+Días de semana salimos a las 11, 
+pero los fines de semana abrimos a las 9
+
+*/
+
+//Entra a un sitio web, para consultar si está abierto hoy...
+
+const dia = 0; //0: domingo... 1: Lunes...
+const horaActual = 10;  
+let mensaje; // está abierto, está cerrado, hoy abrimos a las xx
+
+if ( dia === 0 || dia === 6){
+    
+}
