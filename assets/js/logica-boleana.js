594413893ac7101bf9b955c56517d99d0002b038
Alterlapsus <israel.delen96@gmail.com>
2022-09-21 14:31:10 -0600

    lógica booleana

--- assets/js/logica-boleana.js
+++ assets/js/logica-boleana.js
@@ -26,3 +26,35 @@ console.log( regresaTrue() && regresaFalse() ); //false
 
 console.log('====== && ========');
 regresaFalse() && regresaTrue();
+
+console.log( '4 condiciones', true && true && true && false )// false
+
+console.warn('OR');//true siempre será true si uno de los dos es true, si ambos son false será false
+
+console.log( true || false );
+console.log( false || false );
+
+console.log( regresaTrue() || regresaFalse() );
+
+console.log( regresaFalse() || regresaTrue() );
+console.log( '4 condiciones', true || true || true || false )// true
+
+console.warn('Asignaciones');
+const soyUndefined = undefined; 
+const soyNull = null; 
+const soyFalso = false; 
+
+const a1 = false && 'Hola Mundo' && 150;
+const a2 = 'Hola' && 'Mundo' && soyFalso && true;
+const a3 = soyFalso || 'Ya no soy falso'
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso' || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso' || true;
+
+console.log({a1, a2, a3,a4, a5 });
+
+
+if ( regresaFalse() && regresaTrue() || true || false || true ){
+    console.log('Hacer algo');
+}else{
+    console.log('Hacer otra cosa');
+}
