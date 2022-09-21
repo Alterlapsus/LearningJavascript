31fd5b255a40d05e48fd679d27e7c86686622bc4
Alterlapsus <israel.delen96@gmail.com>
2022-09-21 06:38:09 -0600

    valor referencia

--- assets/js/valor-referencia.js
+++ assets/js/valor-referencia.js
@@ -9,9 +9,33 @@ a = 30;
 console.log({ a,b });
 
 let israel = {nombre: 'Israel'};
-let ana    = israel;
+let ana    = { ...israel };
 ana.nombre = 'Ana';
 
 console.log({israel, ana});
 
-const cambiaNombre = ( persona )
+const cambiaNombre = ( persona ) => {
+    persona.nombre = 'Tony';
+    return persona;
+}
+
+let peter = { nombre: 'Peter'};
+let tony  = cambiaNombre( peter );
+
+console.log({ peter, tony });
+
+//arreglos
+
+const frutas = ['Manzana', 'Pera', 'Piña'];
+
+console.time('slice');
+const otrasFrutas = frutas.slice();
+console.timeEnd('slice');
+
+console.time('spread');
+const otrasFrutas2 = [...frutas];
+console.timeEnd('spread');
+
+otrasFrutas.push('Mango');
+
+console.table({frutas, otrasFrutas});
