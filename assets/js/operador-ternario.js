c8580d468bae3e50c3fd41e83354bd2bfd12873f
Alterlapsus <israel.delen96@gmail.com>
2022-09-21 16:21:22 -0600

    Ejercicio con operadores ternarios

--- assets/js/operador-ternario.js
+++ assets/js/operador-ternario.js
@@ -1,15 +1,29 @@
 /*
-Días de semana salimos a las 11, 
+Días de semana abrirmos a las 11, 
 pero los fines de semana abrimos a las 9
 
 */
 
 //Entra a un sitio web, para consultar si está abierto hoy...
 
-const dia = 0; //0: domingo... 1: Lunes...
+const dia = 1; //0: domingo... 1: Lunes...
 const horaActual = 10;  
+
+let horaApertura;
 let mensaje; // está abierto, está cerrado, hoy abrimos a las xx
 
 if ( dia === 0 || dia === 6){
-    
+    console.log('Fin de semana');
+    horaApertura = 9;
+}else{
+    console.log('Día de la semana');
+    horaApertura = 11;
 }
+
+if ( horaActual >= horaApertura ){
+    mensaje = 'Está abierto';
+}else{
+    mensaje = `Está ceraddo, hoy abrimos a las ${ horaApertura }`;
+}
+
+console.log({ horaApertura, mensaje });
