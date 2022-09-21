62d796a6b8daba9e4d652f2205e125dd3251c61a
Alterlapsus <israel.delen96@gmail.com>
2022-09-21 10:07:52 -0600

    Ejercicio de objetos y arrays

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -16,7 +16,7 @@ const hoy = new Date();
 let dia = hoy.getDay();//0: Domingo, 1: Lunes, 2: Martes... 
 console.log({dia});
 
-if ( dia === '0' ){
+if ( dia === 0 ){
     console.log('Domingo');
 }else if ( dia === 1 ){
     console.log('Lunes');
@@ -36,3 +36,27 @@ if ( dia === '0' ){
 //dia == 0 quiero confirmar si el día es igual al valor 1 pero no me importa el tipo es decir que no importa si es un número un string
 //dia === evalua que no solo los valores internos sean los mismos si no que también el tipado de los mismos tiene que ser identico. Compara si realmente es un número con número o string con string 
 
+// Sin usar if else, o swtich, únicamente objetos o arrays para realizar la misma tarea e imprimir en consola el día de la semana
+
+// utilizando un objeto
+
+dia = 5; //0: domingo, 1: Lunes...
+
+const diasLetras = {
+   0: 'Domingo',    
+   1: 'Lunes',
+   2: 'Martes',
+   3: 'Miercoles',
+   4: 'Jueves',
+   5: 'Viernes',
+   6: 'Sabado' ,
+}
+
+console.log(diasLetras[dia]);
+
+
+// utilizando un array
+
+const diasLetras2 = ['Domingo','Lunes','Martes','Miercoles','Jueves','Sabado', 'Domingo'];
+
+console.log( diasLetras2[dia]);
