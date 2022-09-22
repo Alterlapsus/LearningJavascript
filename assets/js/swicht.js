d2ea40377560d31fcb05714059f17fded5b08ae2
Alterlapsus <israel.delen96@gmail.com>
2022-09-22 09:50:19 -0600

    entendiendo ciclo switch

---
+++ assets/js/swicht.js
@@ -0,0 +1,32 @@
+
+
+const dia = 0; //0: Domingo
+
+switch ( dia ){
+
+    case 0:
+        console.log('Domingo'); 
+        break;
+
+    case 1:
+        console.log('Lunes');
+        
+    case 2:
+        console.log('Martes');
+        
+    case 3:
+        console.log('Miercoles');
+        
+    case 4:
+        console.log('Jueves');
+        
+    case 5:
+        console.log('Viernes');
+        
+    case 6:
+        console.log('Sábado');
+        
+}
+
+
+
