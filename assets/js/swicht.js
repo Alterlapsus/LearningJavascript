6903ff36253fba83ccf2d0aef4199778c57f3bf4
Israel De León <israel.zacarias@plasticosuni.com>
2022-09-22 12:36:15 -0600

    switch dias de la semana

--- assets/js/swicht.js
+++ assets/js/swicht.js
@@ -1,6 +1,7 @@
 
+// === evalua el valor, como también que sea el mismo tipo de dato
 
-const dia = 0; //0: Domingo
+const dia = 2; //0: Domingo
 
 switch ( dia ){
 
@@ -10,21 +11,23 @@ switch ( dia ){
 
     case 1:
         console.log('Lunes');
-        
+        break;
     case 2:
         console.log('Martes');
-        
+        break;
     case 3:
         console.log('Miercoles');
-        
+        break;
     case 4:
         console.log('Jueves');
-        
+        break;
     case 5:
         console.log('Viernes');
-        
+        break;
     case 6:
         console.log('Sábado');
+        default: 
+        console.log('No es Lunes, Martes, Miercoles, Jueves, Viernes, Sábado o Domingo');
         
 }
 
